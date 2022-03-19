d9f09f0115a2542aada94ccfb4d1a9b824dcaaef
Fabian Lewis <49647154+frobian21@users.noreply.github.com>
2022-03-19 00:26:09 +0000

    Add exception for error cases, refactor method names, and fix list summation.

--- app/clients/codingchallenge1.scala
+++ app/clients/codingchallenge1.scala
@@ -3,6 +3,10 @@ package clients
 class codingchallenge1 {
 
   def makePastry(targetLayers: Int): List[LayerOperation] = {
+    //TODO Do with binary number
+    // It would've been easier and faster to convert the number to binary and then ran the fold operation to collect the actions. (0 == even => fold, 1 => add).
+    // The digit with the highest value in a binary number always has a value of 1 in standard form, so we could either always do an add, or ignore it, and do a fold.
+    // This reflects the fact you can go from 1 to 2 layers with a fold or with an add.
     val initialValue = LayerValue(targetLayers, List.empty[LayerOperation])
 
     undoFolding(initialValue).previousSteps.reverse.foldRight(List.empty[LayerOperation]) {
